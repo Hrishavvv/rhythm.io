8d78ca427abdf783df4faf96e7b7dc5236bfae3e
hrishavvv <hrishavsanyal802@gmail.com>
2025-05-07 23:53:50 +0530

    src

--- custom.js
+++ custom.js
@@ -70,7 +70,85 @@ window.customSongs = [
         image: [
             { quality: "500x500", url: "https://a10.gaanacdn.com/gn_img/albums/R7vKX66Wmr/7vKXyp9V3m/size_xl.jpg" }
         ]
+    },
+    {
+        name: "Girls Like You",
+        artists: { primary: [{ name: "Maroon 5" }] },
+        downloadUrl: [
+            { quality: "320kbps", url: "https://streams.gaana.com/songs/38/2142738/23551964/23551964_320.mp4?streamauth=1746814322_cb2f21aa6ae71cd75105dd0ba8f71b94" }
+        ],
+        image: [
+            { quality: "500x500", url: "https://c.saavncdn.com/016/Girls-Like-You-English-2018-20180529030200-500x500.jpg" }
+        ]
+    },
+    {
+        name: "Choo Lo",
+        artists: { primary: [{ name: "The Local Train" }] },
+        downloadUrl: [
+            { quality: "320kbps", url: "https://streams.gaana.com/songs/1/8178201/54218220/54218220_320.mp4?streamauth=1746814435_2ca3e65b4c01e3bb1d1a4285b74f5aee" }
+        ],
+        image: [
+            { quality: "500x500", url: "https://a10.gaanacdn.com/gn_img/albums/R7vKX6WmrP/vKXrNG70Wm/size_xl.jpg" }
+        ]
+    },
+    {
+        name: "Aaoge Tum Kabhi",
+        artists: { primary: [{ name: "The Local Train" }] },
+        downloadUrl: [
+            { quality: "320kbps", url: "https://streams.gaana.com/songs/1/8178201/54218217/54218217_320.mp4?streamauth=1746814474_3dce569e98ccc42b532a40a51fa6e7be" }
+        ],
+        image: [
+            { quality: "500x500", url: "https://c.saavncdn.com/111/Aalas-Ka-Pedh-Hindi-2015-20231005005130-500x500.jpg" }
+        ]
+    },
+    {
+        name: "Dil Diyan Gallan",
+        artists: { primary: [{ name: "Atif Aslam" }] },
+        downloadUrl: [
+            { quality: "320kbps", url: "https://streams.gaana.com/songs/91/2030791/22825334/22825334_320.mp4?streamauth=1746814587_0fd68cf75eeb66068897be17c663bdfb" }
+        ],
+        image: [
+            { quality: "500x500", url: "https://a10.gaanacdn.com/gn_img/song/2lV3d1WRgB/V3d56QQoWR/size_xl_1516003439.jpg" }
+        ]
+    },
+    {
+        name: "Aadat",
+        artists: { primary: [{ name: "Atif Aslam" }] },
+        downloadUrl: [
+            { quality: "320kbps", url: "https://streams.gaana.com/songs/60/24860/1618364/1618364_320.mp4?streamauth=1746814631_2190f8c8c3cc263d7653eb3300a479cf" }
+        ],
+        image: [
+            { quality: "500x500", url: "https://a10.gaanacdn.com/gn_img/albums/oAJbDElKnL/AJbDqOvKnL/size_xl_1713520239.jpg" }
+        ]
+    },
+    {
+        name: "Jeena Jeena",
+        artists: { primary: [{ name: "Atif Aslam" }] },
+        downloadUrl: [
+            { quality: "320kbps", url: "https://streams.gaana.com/songs/63/5385563/41698129/41698129_320.mp4?streamauth=1746814709_0c07f1880ef38b71920b0cdb867fa707" }
+        ],
+        image: [
+            { quality: "500x500", url: "https://a10.gaanacdn.com/gn_img/albums/Bp1bAynb02/1bAynzP4b0/size_xl.jpg" }
+        ]
+    },
+    {
+        name: "O Saathi",
+        artists: { primary: [{ name: "Atif Aslam" }] },
+        downloadUrl: [
+            { quality: "320kbps", url: "https://streams.gaana.com/songs/66/2080466/23164124/23164124_320.mp4?streamauth=1746814759_1383e637aa05851f8297ad1984ae26c6" }
+        ],
+        image: [
+            { quality: "500x500", url: "https://a10.gaanacdn.com/gn_img/song/zLp36v3rGe/p36VLRXE3r/size_xl_1520584095.jpg" }
+        ]
+    },
+    {
+        name: "Rang Jo Lagyo",
+        artists: { primary: [{ name: "Atif Aslam" }] },
+        downloadUrl: [
+            { quality: "320kbps", url: "https://streams.gaana.com/songs/91/2148991/23591822/23591822_320.mp4?streamauth=1746814868_3e8966da94a21fc170d3bf30814866a0" }
+        ],
+        image: [
+            { quality: "500x500", url: "https://a10.gaanacdn.com/gn_img/albums/4Z9bqZoKyQ/Z9bq0qYXWy/size_xl_1713881663.jpg" }
+        ]
     }
-
-    
 ];
